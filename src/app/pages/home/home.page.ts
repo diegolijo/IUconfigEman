731bd0058567719eec7cd435a45d5600e7f29ec9
diegolijo <diegolijo@gmail.com>
2021-02-04 22:12:57 +0100

    login

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,33 +9,46 @@ const { NatPlugin } = Plugins;
 })
 export class HomePage {
 
-  public submitted: boolean;
-  public password: string;
-  public version: string;
   public username: string;
-
+  public password: string;
+  public submitted: boolean;
+  public version: string;
+  public visibility = 'password';
 
   constructor() {
 
-    this.submitted = true;
-    this.password = '';
-    this.version = '';
     this.username = '';
+    this.password = '';
+    this.submitted = true;
+    this.version = '';
   }
 
   public async toAndroidService() {
-    debugger;
+    //  debugger;
     const result = await NatPlugin.customCall({ message: 'CUSTOM MESSAGE' });
     this.username = result;
     this.submitted = result;
-    this.version =result;
+    this.version = result;
     const res = await NatPlugin.getPalabra({ message: 'CUSTOM MESSAGE' });
   }
 
 
+  switchVisibility() {
+    switch (this.visibility) {
+      case 'text':
+        this.visibility = 'password';
+        break;
+      case 'password':
+        this.visibility = 'text';
+        break;
+      default:
+        break;
+    }
+  }
 
+  getVisibility() {
 
-
+  }
 
 
 
